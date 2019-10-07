af4883f6820d2c334c0b977971a081f8c1c0e649
tomaszbielenin <56257769+tomaszbielenin@users.noreply.github.com>
2019-10-07 13:46:01 +0200

    Add files via upload

---
+++ BackUp.bat
@@ -0,0 +1,26 @@
+REM 1. Usuń stare pliki - oszczedzaj miejsce na dysku
+REM 2. Robocopy nowe wersje, logfile - oszczedza transfer
+REM 3. Spakuj i przenieś do archiwum- zaraz po replikacji, żeby data się zgadzała
+
+:setVariables
+REM Set %mydate% for further use in file naming
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
+For /f "tokens=1-2 delims=/:" %%a in ("%time%") do (set mytime=%%a%%b)
+REM echo %mydate%_%mytime%
+REM Set destination folder variable - %dFolder%
+set dFolder=\\krvfpp01\Proj\60R18506KRV\DESIGN\REFSECURE\RVT\RevitBackup
+REM echo %dFolder%
+
+:delete
+For /d %%d in ("%dFolder%\*") do ROBOCOPY "%%d\archive" "%temp%\~robodel" *.zip /mov /minage:30
+del "%temp%\~robodel" /q
+
+:robocopy
+REM Robocopy compares files last modified date in source and destination folder and updates out of date copies
+For /d %%d in ("%dFolder%\*") do If not exist "%%d\log" mkdir "%%d\log"
+For /d %%d in ("%dFolder%\*") do ROBOCOPY "\\Gbwni0vs01\JMBM\BIM\01-WIP\BIM-AC\Revit Model\%%~nd" "%%d" *.rvt /log:"%%d\log\%mydate%_%mytime%_%%~nd.log"
+
+:archive
+For /d %%d in ("%dFolder%\*") do If not exist "%%d\archive" mkdir "%%d\archive"
+For /d %%d in ("%dFolder%\*") do "C:\Program Files\7-Zip\7z.exe" a -tzip  "%%d\archive\%mydate%_%mytime%_%%~nd.zip" "%%d\*.rvt"
+REM Pause
